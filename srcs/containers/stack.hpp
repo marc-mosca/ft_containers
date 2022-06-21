97542e47a6dadd409570de147e516df42d70234f
marc-mosca <marc.mosca@icloud.com>
2022-06-21 17:57:02 +0200

    feat: empty and size functions of stack container

--- srcs/containers/stack.hpp
+++ srcs/containers/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 13:15:54 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/21 14:15:20 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/06/21 17:17:19 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,61 @@
 namespace ft
 {
 
-// Don't forget to use ft::vector when it is implemented.
-template < class T, class Container = std::vector<T> >
-class Stack
-{
+	// Don't forget to use ft::vector when it is implemented.
+	template < class T, class Container = std::vector<T> >
+	class Stack
+	{
 
-public:
+		public:
+			/**
+			 *	The first template parameter (T). 
+			 */
+			typedef T	value_type;
 
+			/**
+			 *	The second template parameter (Container).
+			 */
+			typedef Container	container_type;
 
-};
+			/**
+			 *	An unsigned integral type.
+			 */
+			typedef size_t	size_type;
+
+		public:
+			/**
+			 * Construct a new stack object
+			 * 
+			 * @param ctnr container object
+			 */
+			explicit stack( const container_type& ctnr = container_type() ) : ct(ctnr)
+			{
+			}
+
+			/**
+			 * Returns the number of elements in the stack.
+			 * 
+			 * @return The number of elements in the underlying container.
+			 */
+			size_type size() const
+			{
+				return ( this->ct.size() );
+			}
+
+			/**
+			 * Returns wheter the stack is empty.
+			 * 
+			 * @return true if the stack is empty, otherwise, return false
+			 */
+			bool empty() const
+			{
+				return ( (this->size() == 0) ? true : false );
+			}
+
+		protected:
+			container_type	ct;
+
+	};
 
 }
 
--- srcs/tools/pair.hpp
+++ srcs/tools/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 17:17:02 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/21 14:16:41 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/06/21 16:20:59 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ namespace ft
 template < class T1, class T2 >
 struct pair
 {
-	T1 first;
-	T2 second;
+	
 };
 
 }
