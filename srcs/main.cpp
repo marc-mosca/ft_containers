cc1c91b0a9b926e2f2e70858044e9eaf831c1ea0
marc-mosca <marc.mosca@icloud.com>
2022-06-21 14:17:10 +0200

    feat: start pair container

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,10 +6,12 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 13:14:15 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/20 17:16:08 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/06/21 14:12:27 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./containers/map.hpp"
 #include "./containers/stack.hpp"
 #include "./containers/vector.hpp"
+#include "./tools/equal.hpp"
+#include "./tools/pair.hpp"
--- srcs/tools/pair.hpp
+++ srcs/tools/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 17:17:02 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/20 17:21:55 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/06/21 14:16:41 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 namespace ft
 {
 	
+template < class T1, class T2 >
+struct pair
+{
+	T1 first;
+	T2 second;
+};
+
 }
 
 #endif
