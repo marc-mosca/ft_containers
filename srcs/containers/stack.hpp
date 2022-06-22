37676f4e821652704563d7df3836d7ab095519da
marc-mosca <marc.mosca@icloud.com>
2022-06-22 13:23:49 +0200

    feat: member functions of stack done

--- srcs/containers/stack.hpp
+++ srcs/containers/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 13:15:54 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/21 17:17:19 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/06/22 13:21:47 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 namespace ft
 {
 
-	// Don't forget to use ft::vector when it is implemented.
 	template < class T, class Container = std::vector<T> >
 	class Stack
 	{
@@ -27,52 +26,119 @@ namespace ft
 			/**
 			 *	The first template parameter (T). 
 			 */
-			typedef T	value_type;
+			typedef Container::value_type		value_type;
 
 			/**
 			 *	The second template parameter (Container).
 			 */
-			typedef Container	container_type;
+			typedef Container					container_type;
 
 			/**
 			 *	An unsigned integral type.
 			 */
-			typedef size_t	size_type;
+			typedef Container::size_type		size_type;
+
+			/**
+			 * A reference to value_type.
+			 */
+			typedef Container::reference		reference;
+			
+			/**
+			 * A const reference to value_type.
+			 */
+			typedef Container::const_reference	const_reference;
 
 		public:
 			/**
-			 * Construct a new stack object
+			 * Constructs a stack container adaptor object.
 			 * 
 			 * @param ctnr container object
 			 */
-			explicit stack( const container_type& ctnr = container_type() ) : ct(ctnr)
+			explicit stack( const container_type& ctnr = container_type() )
+				: c(ctnr)
 			{
 			}
 
 			/**
-			 * Returns the number of elements in the stack.
-			 * 
-			 * @return The number of elements in the underlying container.
+			 * Destroy the stack object.
 			 */
-			size_type size() const
+			~stack()
 			{
-				return ( this->ct.size() );
 			}
 
 			/**
-			 * Returns wheter the stack is empty.
+			 * Check if the stack is empty or not.
 			 * 
-			 * @return true if the stack is empty, otherwise, return false
+			 * @return if the stack is empty, returns true, otherwise return false
 			 */
 			bool empty() const
 			{
 				return ( (this->size() == 0) ? true : false );
 			}
 
+			/**
+			 * Computes the number of elements in the stack container.
+			 * 
+			 * @return the number of elements in the underlying container
+			 */
+			size_type size() const
+			{
+				return ( this->c.size() );
+			}
+
+			/**
+			 * Search and return a reference to the last element in the
+			 * container.
+			 * 
+			 * @return a reference to the last element in the container
+			 */
+			reference top()
+			{
+				return ( this->c.back() );
+			}
+
+			/**
+			 * Search and return a const reference to the last element in the
+			 * container.
+			 * 
+			 * @return a const reference to the last element in the container
+			 */
+			const_reference top() const
+			{
+				return ( this->c.back() );
+			}
+
+			/**
+			 * Inserts a new element at the top of the stack. The content of
+			 * this new element is initialized to a copy of val.
+			 * 
+			 * @param val value to which the inserted element is initialized
+			 */
+			void push( const_reference val )
+			{
+				this->c.push_back(val);
+			}
+
+			/**
+			 * Removes the top element from the stack.
+			 */
+			void pop()
+			{
+				this->c.pop_back();
+			}
+
 		protected:
-			container_type	ct;
+			/**
+			 * the underlying container.
+			 */
+			container_type	c;
 
 	};
+
+	template < class T, class Container >
+	bool operator==( const ft::stack< T, Container >& lhs, const ft::stack< T, Container >& rhs )
+	{
+	}
 
 }
 
