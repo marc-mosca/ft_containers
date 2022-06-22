88ca69f93d42a0e1e628d60d977ded96cfea8023
marc-mosca <marc.mosca@icloud.com>
2022-06-22 17:21:36 +0200

    feat: start pair

--- srcs/tools/pair.hpp
+++ srcs/tools/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 17:17:02 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/21 16:20:59 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/06/22 17:21:01 by mmosca           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,21 @@
 namespace ft
 {
 	
-template < class T1, class T2 >
-struct pair
-{
-	
-};
+	template < class T1, class T2 >
+	struct pair
+	{
+		
+		//	~~	Members types
+
+		typedef T1	first_type;
+		typedef T2	second_type;
+
+		//	~~	Members objects
+
+		first_type	first;
+		second_type	second;
+
+	};
 
 }
 
