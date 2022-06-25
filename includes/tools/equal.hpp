c8b40b2bbbd0004c7160c2e6a3097ac915ab7ef0
marc-mosca <marc.mosca@icloud.com>
2022-06-25 10:20:04 +0200

    feat: equal function done

--- includes/tools/equal.hpp
+++ includes/tools/equal.hpp
@@ -6,7 +6,7 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 17:17:25 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/20 17:21:39 by mmosca           ###   ########lyon.fr   */
+/*   Updated: 2022/06/25 10:17:27 by mmosca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,25 @@
 
 namespace ft
 {
-	
+
+	template<class InputIt1, class InputIt2>
+	bool equal (InputIt1 first1, InputIt1 last1, InputIt2 first2) {
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!(*first1 == *first2))
+				return false;
+		}
+		return true;
+	}
+
+	template<class InputIt1, class InputIt2, class BinaryPredicate>
+	bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p) {
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!p(*first1, *first2))
+				return false;
+		}
+		return true;
+	}
+
 }
 
 #endif
