8d0cb1c1e7cba58e3f5773c0c91005a20f3cd24d
marc-mosca <marc.mosca@icloud.com>
2022-06-25 20:46:19 +0200

    fix: lexicographical_compare equality fix

--- includes/tools/lexicographical_compare.hpp
+++ includes/tools/lexicographical_compare.hpp
@@ -6,10 +6,9 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 10:20:59 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/25 10:25:12 by mmosca           ###   ########.fr       */
+/*   Updated: 2022/06/25 20:45:31 by mmosca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef LEXICOGRAPHICAL_COMPARE_HPP
 #define LEXICOGRAPHICAL_COMPARE_HPP
@@ -19,10 +18,9 @@ namespace ft {
 	template<class InputIt1, class InputIt2>
 	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2) {
 		for (; (first1 != last1) && (first2 != last2); ++first1, ++first2) {
-			if (*first1 < *first2)
-				return true;
-			if (*first2 < *first1)
-				return false;
+			if (*first1 == *first2)
+				continue;
+			return !(*first2 == *last2 || *first2 < *first1);
 		}
 		return (first1 == last1 && first2 != last2);
 	}
@@ -30,10 +28,9 @@ namespace ft {
 	template<class InputIt1, class InputIt2, class Compare>
 	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare comp) {
 		for (; (first1 != last1) && (first2 != last2); ++first1, ++first2) {
-			if (comp(*first1, *first2))
-				return true;
-			if (comp(*first2, *first1))
-				return false;
+			if (*first1 == *first2)
+				continue;
+			return !(*first2 == *last2 || comp(*first2, *first1));
 		}
 		return (first1 == last1 && first2 != last2);
 	}
