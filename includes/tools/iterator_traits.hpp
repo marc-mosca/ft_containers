2815480d727503e09df5e7764d1cb99f1da802a2
marc-mosca <marc.mosca@icloud.com>
2022-06-27 19:22:28 +0200

    feat: start iterators

--- includes/tools/iterator_traits.hpp
+++ includes/tools/iterator_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 16:04:30 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/27 16:05:35 by mmosca           ###   ########.fr       */
+/*   Updated: 2022/06/27 19:20:47 by mmosca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 
 namespace ft
 {
+	template<class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
+	struct iterator
+	{
+		typedef Category	iterator_category;
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+	};
+
 	template<class Iter>
 	struct iterator_traits
 	{
@@ -23,6 +33,26 @@ namespace ft
 		typedef Iter::pointer			pointer;
 		typedef Iter::reference			reference;
 		typedef Iter::iterator_category	iterator_category;
+	};
+
+	template<class T>
+	struct iterator_traits<T*>
+	{
+		typedef ptrdiff_t					difference_type;
+		typedef T							value_type;
+		typedef T							*pointer;
+		typedef T							&reference;
+		typedef random_access_iterator_tag	iterator_category;
+	};
+
+	template<class T>
+	struct iterator_traits<const T*>
+	{
+		typedef ptrdiff_t					difference_type;
+		typedef T							value_type;
+		typedef const T						*pointer;
+		typedef const T						&reference;
+		typedef random_access_iterator_tag	iterator_category;
 	};
 }
 
--- includes/tools/reverse_iterator.hpp
+++ includes/tools/reverse_iterator.hpp
@@ -6,12 +6,14 @@
 /*   By: mmosca <mmosca@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 15:53:46 by mmosca            #+#    #+#             */
-/*   Updated: 2022/06/27 16:08:34 by mmosca           ###   ########.fr       */
+/*   Updated: 2022/06/27 17:14:03 by mmosca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REVERSE_ITERATOR_HPP
 #define REVERSE_ITERATOR_HPP
+
+#include "iterator_traits.hpp"
 
 namespace ft
 {
@@ -27,6 +29,16 @@ namespace ft
 			typedef ft::iterator_traits<Iter>::difference_type		difference_type;
 			typedef ft::iterator_traits<Iter>::pointer				pointer;
 			typedef ft::iterator_traits<Iter>::reference			reference;
+
+			//	~~	Member functions -------------------------------------------
+			reverse_iterator();
+			explicit reverse_iterator(iterator_type x);
+			template<class U>
+			reverse_iterator(const reverse_iterator<U> &other);
+			~reverse_iterator();
+			template<class U>
+			reverse_iterator &operator=(const reverse_iterator<U> &other);
+			iterator_type base() const;
 
 		protected:
 			//	~~	Member objects ---------------------------------------------
